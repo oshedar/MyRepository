e53c8dc1140d7695ec4fbf33786c5f118c82039a
Snehal <snehal.m@fuzicom.com>
2015-04-29 14:09:52 +0530

    Added basic UI.

---
+++ DummyProject/Assets/Scripts/BasicUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+		GUI.skin.label.fontSize = 30;
+		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "Test Successful");
+	}
+}
