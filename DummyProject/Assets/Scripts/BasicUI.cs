37a54355679548752f992ea7a00ecdf59cd6b653
Snehal <snehal.m@fuzicom.com>
2015-04-29 14:39:32 +0530

    committing my changes

--- DummyProject/Assets/Scripts/BasicUI.cs
+++ DummyProject/Assets/Scripts/BasicUI.cs
@@ -18,5 +18,8 @@ public class BasicUI : MonoBehaviour {
 		GUI.skin.label.alignment = TextAnchor.MiddleCenter;
 		GUI.skin.label.fontSize = 30;
 		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "Test Successful");
+		GUI.Label (new Rect (0, -100, Screen.width, Screen.height), "Test 1 Snehal Successful");
+		GUI.Label (new Rect (0, 200, Screen.width, Screen.height), "Test 2 Sachet Successful");
+
 	}
 }
